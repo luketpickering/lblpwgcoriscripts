b5d84f0ae391b43e985630a1ef060fc48303fc21
Luke Pickering <luketpickering@googlemail.com>
2019-06-27 17:24:46 -0500

    Tidies scripts, now can configure scripts for either make_all_throws or make_toy_throws from the same helper. Jobnames configurable and added to dir structure.

--- checkpoint.sh
+++ checkpoint.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 
-CHECKFREQ_S=${1}
-OUTDIR=${2}
-NODETMP=${3}
+EXENAME=${1}
+CHECKFREQ_S=${2}
+OUTDIR=${3}
+NODETMP=${4}
 CHK_COUNT=0
-
-CHKFILE=${NODETMP}/chk_toy_fit_${SLURM_NODEID}.root
 
 cd ${NODETMP}
 
+CHKFILE=chk_fit_j${SLURM_JOB_ID}_n${SLURM_NODEID}.root
+
 function check_live() {
-  nmtt=$(ps aux | grep "make_toy_throws" | grep -v "grep" | grep -v "srun" | grep -v "bash" | wc -l)
-  echo "[CHK-INFO]: There are ${nmtt} make_toy_throws still running @ $(date '+%Y_%m_%d-%H_%M_%S')"
-  if [ "${nmtt}" == "0" ]; then
+  NRELPROCS=$(ps aux | grep "${EXENAME}" | grep -v "grep" | grep -v "srun" | grep -v "bash" | wc -l)
+  echo "[CHK-INFO]: There are ${NRELPROCS} ${EXENAME} still running @ $(date '+%Y_%m_%d-%H_%M_%S')"
+  if [ "${NRELPROCS}" == "0" ]; then
     echo "[CHK-INFO]: Finished! No processes running, cleaning up /dev/shm and bailing @ $(date '+%Y_%m_%d-%H_%M_%S')"
     rm -f ${CHKFILE}
-    
-    FILE_COUNT=$(ls -1q ${NODETMP}/fit_*.root 2>/dev/null | wc -l)
+
+    FILE_COUNT=$(ls -1q fit_*.root 2>/dev/null | wc -l)
     echo "[CHK-INFO]: Found ${FILE_COUNT} output files... hadding"
     touch ${CAFANA_CHK_SEMAPHORE}
-    hadd -k ${CHKFILE} ${NODETMP}/toy_fit_*.root
+    hadd -k ${CHKFILE} fit_*.root
     rm ${CAFANA_CHK_SEMAPHORE}
-    
-    mkdir -p ${OUTDIR}/
-    echo "[CHK-INFO]: Copying out \"cp ${CHKFILE} ${OUTDIR}/toy_fit_${SLURM_NODEID}.final.root\""
-    cp ${CHKFILE} ${OUTDIR}/toy_fit_${SLURM_NODEID}.final.root
 
-    FILE_COUNT=$(ls -1q ${NODETMP}/*.log 2>/dev/null | wc -l)
-    echo "[CHK-INFO]: Copying out \"cp toy_log_${SLURM_NODEID}.final.tar.gz ${OUTDIR}/\""
+    mkdir -p ${OUTDIR}/
+    echo "[CHK-INFO]: Copying out \"cp ${CHKFILE} ${OUTDIR}/fit_j${SLURM_JOB_ID}_n${SLURM_NODEID}.final.root\""
+    cp ${CHKFILE} ${OUTDIR}/fit_j${SLURM_JOB_ID}_n${SLURM_NODEID}.final.root
+
+    FILE_COUNT=$(ls -1q *.log 2>/dev/null | wc -l)
+    echo "[CHK-INFO]: Copying out \"cp log_j${SLURM_JOB_ID}_n${SLURM_NODEID}.final.tar.gz ${OUTDIR}/\""
     # Tar should shut up as itz zipping up the file that its writing out to
-    tar -zcf toy_log_${SLURM_NODEID}.final.tar.gz *.log 1>/dev/null 2>&1
-    cp toy_log_${SLURM_NODEID}.final.tar.gz ${OUTDIR}/
+    tar -zcf log_j${SLURM_JOB_ID}_n${SLURM_NODEID}.final.tar.gz *.log 1>/dev/null 2>&1
+    cp log_j${SLURM_JOB_ID}_n${SLURM_NODEID}.final.tar.gz ${OUTDIR}/
     rm -rf ${NODETMP}
     exit 0
   fi
@@ -40,31 +41,30 @@ function make_checkpoint() {
   check_live
   echo "[CHK-INFO]: Checking for output root files @ $(date '+%Y_%m_%d-%H_%M_%S')"
   rm -f ${CHKFILE}
-  
+
   touch ${CAFANA_CHK_SEMAPHORE}
-  FILE_COUNT=$(ls -1q ${NODETMP}/fit_*.root 2>/dev/null | wc -l)
+  FILE_COUNT=$(ls -1q fit_*.root 2>/dev/null | wc -l)
   echo "[CHK-INFO]: Found ${FILE_COUNT} output files..."
   if [ ${FILE_COUNT} -gt 0 ]; then
-    hadd -k ${CHKFILE} ${NODETMP}/toy_fit_*.root
-    echo "[CHK-INFO]: Copying out \"cp ${CHKFILE} ${OUTDIR}/toy_fit_${SLURM_NODEID}.chk_${CHK_COUNT}.root\""
-    cp ${CHKFILE} ${OUTDIR}/toy_fit_${SLURM_NODEID}.chk_${CHK_COUNT}.root
+    hadd -k ${CHKFILE} fit_*.root
+    echo "[CHK-INFO]: Copying out \"cp ${CHKFILE} ${OUTDIR}/fit_j${SLURM_JOB_ID}_n${SLURM_NODEID}.chk_${CHK_COUNT}.root\""
+    cp ${CHKFILE} ${OUTDIR}/fit_j${SLURM_JOB_ID}_n${SLURM_NODEID}.chk_${CHK_COUNT}.root
     rm -f ${CHKFILE}
   fi
   rm ${CAFANA_CHK_SEMAPHORE}
-  
-  FILE_COUNT=$(ls -1q ${NODETMP}/*.log 2>/dev/null | wc -l)  
+
+  FILE_COUNT=$(ls -1q *.log 2>/dev/null | wc -l)
   echo "[CHK-INFO]: Found ${FILE_COUNT} log files..."
   if [ ${FILE_COUNT} -gt 0 ]; then
     # Tar should shut up as itz zipping up the file that its writing out to
-    tar -zcf log_${SLURM_NODEID}.chk_${CHK_COUNT}.tar.gz *.log 1>/dev/null 2>&1
-    echo "[CHK-INFO]: Copying out \"cp toy_log_${SLURM_NODEID}.chk_${CHK_COUNT}.tar.gz ${OUTDIR}/\""
-    cp toy_log_${SLURM_NODEID}.chk_${CHK_COUNT}.tar.gz ${OUTDIR}/
-    rm -f toy_log_${SLURM_NODEID}.chk_${CHK_COUNT}.tar.gz
+    tar -zcf log_j${SLURM_JOB_ID}_n${SLURM_NODEID}.chk_${CHK_COUNT}.tar.gz *.log 1>/dev/null 2>&1
+    echo "[CHK-INFO]: Copying out \"cp log_j${SLURM_JOB_ID}_n${SLURM_NODEID}.chk_${CHK_COUNT}.tar.gz ${OUTDIR}/\""
+    cp log_j${SLURM_JOB_ID}_n${SLURM_NODEID}.chk_${CHK_COUNT}.tar.gz ${OUTDIR}/
+    rm -f log_j${SLURM_JOB_ID}_n${SLURM_NODEID}.chk_${CHK_COUNT}.tar.gz
   fi
   echo "[CHK-INFO]: Done checkpoint ${CHK_COUNT}"
   CHK_COUNT=$(( CHK_COUNT + 1 ))
 }
-
 
 echo "[CHK-INFO]: Let's do some checkpointing on job ${SLURM_JOB_ID} node ${SLURM_NODEID}. Checkpointing every ${CHECKFREQ_S} seconds @ $(date '+%Y_%m_%d-%H_%M_%S')"
 
@@ -77,5 +77,3 @@ while true; do
   sleep ${DECICHECKFREQ_S}
   make_checkpoint
 done
-
-
--- run_throws.sh
+++ run_throws.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
-TIME_REQ_M=$1
-UNITSAFE_M=$2
-OUTDIR=$3
-SYSTLIST=$4
-SAMPLELIST=$5
-THROWTYPE=$6
-PENALTY=$7
-HIERARCHY=$8
-OSCVARS=$9
+CAFEEXE=${1}
+TIME_REQ_M=${2}
+UNITSAFE_M=${3}
+OUTDIR=${4}
+SYSTLIST=${5}
+SAMPLELIST=${6}
+THROWTYPE=${7}
+PENALTY=${8}
+HIERARCHY=${9}
+OSCVARS=${10}
 
+#Checkpointing settings:
+export CAFANA_TOTALDURATION_MIN=${TIME_REQ_M}
+export CAFANA_CHKDURATION_MIN=60
+export CAFANA_SAFEUNITDURATION_MIN=${UNITSAFE_M}
+export CAFANA_CHK_SEMAPHORE=${NODETMP}/hadd.smph
+
+#Checkpoint every half an hour, check NProcs every 3 mins.
+CHKSCRIPT_FREQ_S=$(( 30 * 60 ))
+
+# Write all output straight to the shared memory filesystem
 TMPDIR=/dev/shm
+#Can use your projectdir for debugging, but will be problematic at scale
 #TMPDIR=/project/projectdirs/dune/users/${USER}/jobtmp
 JOBTMP=${TMPDIR}/${SLURM_JOB_ID}
 NODETMP=${JOBTMP}/n_${SLURM_NODEID}
+
 mkdir -p ${NODETMP}
-LOGFILE=${NODETMP}/toy_job_${SLURM_JOB_ID}_n${SLURM_NODEID}_l${SLURM_LOCALID}.log
-FITFILE=${NODETMP}/toy_fit_${SLURM_LOCALID}.root
+
+LOGFILE=${NODETMP}/job_${SLURM_JOB_ID}_n${SLURM_NODEID}_l${SLURM_LOCALID}.log
+FITFILE=${NODETMP}/fit_${SLURM_LOCALID}.root
 
 echo "Running process ${SLURM_PROCID} of job ${SLURM_JOB_ID} on node ${SLURM_NODEID} local ID ${SLURM_LOCALID} @ $(date '+%Y_%m_%d-%H_%M_%S')" 2>&1 | tee ${LOGFILE}
 
@@ -25,40 +39,54 @@ STATESTUB="/statefiles/State"
 #We use the timing so just set this to 1
 NTHROWS=1
 
-# Add one to the seed so that the first proc has seed == 1 (seed of 0 uses the time)
-SEED=$(( SLURM_PROCID + 1 ))
-
-OPTS="${SEED} ${STATESTUB} ${FITFILE} ${NTHROWS} ${SYSTLIST} ${SAMPLELIST} ${THROWTYPE} ${PENALTY} ${HIERARCHY} 0 ${OSCVARS}"
 echo "[DEBUG]: Sourcing environment @ $(date '+%Y_%m_%d-%H_%M_%S')" 2>&1 | tee -a ${LOGFILE}
 source /opt/CAFAna/CAFAnaEnv.sh 2>&1 | tee -a ${LOGFILE}
+printenv |  tee -a ${LOGFILE}
 
 echo "[INFO]: df -h ${TMPDIR}" | tee -a ${LOGFILE}
 df -h ${TMPDIR} | tee -a ${LOGFILE}
 
-
-export CAFANA_TOTALDURATION_MIN=${TIME_REQ_M}
-export CAFANA_CHKDURATION_MIN=$(( TIME_REQ_M / 10 ))
-export CAFANA_SAFEUNITDURATION_MIN=${UNITSAFE_M}
-export CAFANA_CHK_SEMAPHORE=${NODETMP}/hadd.smph
-
-CHKSCRIPT_FREQ_S=$(( CAFANA_CHKDURATION_MIN * 60 ))
-
 echo "[DEBUG]: CHK: CAFANA_TOTALDURATION_MIN=${CAFANA_TOTALDURATION_MIN}, CAFANA_CHKDURATION_MIN=${CAFANA_CHKDURATION_MIN}" 2>&1 | tee -a ${LOGFILE}
 echo "[DEBUG]: CHK: CAFANA_SAFEUNITDURATION_MIN=${CAFANA_SAFEUNITDURATION_MIN}, CAFANA_CHK_SEMAPHORE=${CAFANA_CHK_SEMAPHORE}" 2>&1 | tee -a ${LOGFILE}
 
+EXENAME="${CAFEEXE}_fixed_seed"
+echo "[DEBUG]: Running ${EXENAME}"
 
+#This script should be checkpointing
 if [ "${SLURM_LOCALID}" == "0" ]; then
-     echo "I am local id 0, backgrounding checkpointing script which should wake up ever ${CHKSCRIPT_FREQ_S} s." 2>&1 | tee -a ${LOGFILE}
-    ./toy_checkpoint.sh ${CHKSCRIPT_FREQ_S} ${OUTDIR} ${NODETMP} > >(tee -a ${LOGFILE}) 2>&1 &
+     echo "[CHK-INFO]: I am local id 0, backgrounding checkpointing script which should wake up every ${CHKSCRIPT_FREQ_S} s." 2>&1 | tee -a ${LOGFILE}
+    ./checkpoint.sh ${EXENAME} ${CHKSCRIPT_FREQ_S} ${OUTDIR} ${NODETMP} > >(tee -a ${LOGFILE}) 2>&1 &
 fi
 
-echo "make_toy_throws_fixed_seed ${OPTS} @ $(date '+%Y_%m_%d-%H_%M_%S')" 2>&1 | tee -a ${LOGFILE}
-make_toy_throws_fixed_seed ${OPTS} 2>&1 | tee -a ${LOGFILE}
+# Add one to the seed so that the first proc has seed == 1 (seed of 0 uses the time)
+SEED=$(( SLURM_PROCID + 1 ))
+echo "[DEBUG]: Seed = ${SEED}"
+
+if [ "${EXENAME}" == "make_all_throws_fixed_seed" ]; then
+
+  OPTS="${SEED} ${STATESTUB} ${FITFILE} ${NTHROWS} ${SYSTLIST} ${SAMPLELIST} ${THROWTYPE} ${PENALTY} ${HIERARCHY}"
+
+  echo "[RUN]: make_all_fixed_seed ${OPTS} @ $(date '+%Y_%m_%d-%H_%M_%S')" 2>&1 | tee -a ${LOGFILE}
+  make_all_fixed_seed ${OPTS} 2>&1 | tee -a ${LOGFILE}
+
+elif [ "${EXENAME}" == "make_toy_throws_fixed_seed" ]; then
+
+  #Only matters for limited osc parameter throws, always start from NuFit4
+  ASIMOV_SET="0"
+
+  OPTS="${SEED} ${STATESTUB} ${FITFILE} ${NTHROWS} ${SYSTLIST} ${SAMPLELIST} ${THROWTYPE} ${PENALTY} ${HIERARCHY} ${ASIMOV_SET} ${OSCVARS}"
+
+  echo "[RUN]: make_toy_throws_fixed_seed ${OPTS} @ $(date '+%Y_%m_%d-%H_%M_%S')" 2>&1 | tee -a ${LOGFILE}
+  make_toy_throws_fixed_seed ${OPTS} 2>&1 | tee -a ${LOGFILE}
+
+fi
+
 echo "DONE @ $(date '+%Y_%m_%d-%H_%M_%S')" | tee -a ${LOGFILE}
+
 
 #Don't end the script until checkpoint is done
 if [ "${SLURM_LOCALID}" == "0" ]; then
-  echo "[INFO]: Waiting on toy_checkpoint.sh" | tee -a ${LOGFILE}
+  echo "[INFO]: Waiting on checkpoint.sh" | tee -a ${LOGFILE}
   jobs | tee -a ${LOGFILE}
 fi
 
