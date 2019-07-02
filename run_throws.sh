e8ca5e899f183a2ad2dde23567ad57e58c092c04
Luke Pickering <luketpickering@googlemail.com>
2019-07-02 11:55:37 -0700

    Fixes typo in make_all_throws_fixed_seed usage. Updates image to include latest meta tree output

--- run_throws.sh
+++ run_throws.sh
@@ -68,8 +68,8 @@ if [ "${EXENAME}" == "make_all_throws_fixed_seed" ]; then
 
   OPTS="${SEED} ${STATESTUB} ${FITFILE} ${NTHROWS} ${SYSTLIST} ${SAMPLELIST} ${THROWTYPE} ${PENALTY} ${HIERARCHY}"
 
-  echo "[RUN]: make_all_fixed_seed ${OPTS} @ $(date '+%Y_%m_%d-%H_%M_%S')" 2>&1 | tee -a ${LOGFILE}
-  make_all_fixed_seed ${OPTS} 2>&1 | tee -a ${LOGFILE}
+  echo "[RUN]: make_all_throws_fixed_seed ${OPTS} @ $(date '+%Y_%m_%d-%H_%M_%S')" 2>&1 | tee -a ${LOGFILE}
+  make_all_throws_fixed_seed ${OPTS} 2>&1 | tee -a ${LOGFILE}
 
 elif [ "${EXENAME}" == "make_toy_throws_fixed_seed" ]; then
 
