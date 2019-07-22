a49368e011dbd4268de8cc853a4c39365a6236c7
Luke Pickering <luketpickering@googlemail.com>
2019-07-22 14:49:09 -0500

    root is stupud

--- cat_asimovs.C
+++ cat_asimovs.C
@@ -1,27 +1,40 @@
-void cat_asimovs(char const *outputfile, char const *inputfile, char const *inputhisto, char const *varsdir,char const *expdir,char const *asmdir){
+void cat_asimovs(char const *outputfile, char const *inputfile,
+                 char const *inputhisto, char const *varsdir,
+                 char const *expdir, char const *asmdir) {
 
-  TFile *fin = TFile::Open(inputfile,"READ");
+  TFile *fin = TFile::Open(inputfile, "READ");
   TH2 *hin;
   fin->GetObject(inputhisto, hin);
 
-  if(!hin){
-    std::cout << "Failed to find " << inputhisto << " in " << inputfile << std::endl;
+  if (!hin) {
+    std::cout << "Failed to find " << inputhisto << " in " << inputfile
+              << std::endl;
     return;
   }
 
-  hin = (TH2*)hin->Clone();
+  hin = (TH2 *)hin->Clone();
   hin->SetDirectory(nullptr);
 
-  TFile *fout = TFile::Open(outputfile,"UPDATE");
+  TFile *fout = TFile::Open(outputfile, "UPDATE");
   fout->cd();
 
-  gDirectory->mkdir(varsdir)->cd();
-  gDirectory->mkdir(expdir)->cd();
-  gDirectory->mkdir(asmdir)->cd();
+  if (!gDirectory->GetDirectory(varsdir)) {
+    gDirectory->mkdir(varsdir)
+  }
+  gDirectory->GetDirectory(varsdir)->cd();
+
+  if (!gDirectory->GetDirectory(expdir)) {
+    gDirectory->mkdir(expdir)
+  }
+  gDirectory->GetDirectory(expdir)->cd();
+
+  if (!gDirectory->GetDirectory(asmdir)) {
+    gDirectory->mkdir(asmdir)
+  }
+  gDirectory->GetDirectory(asmdir)->cd();
 
   gDirectory->WriteTObject(hin);
 
   fout->Write();
   fout->Close();
-
 }
